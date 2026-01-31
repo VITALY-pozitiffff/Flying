06f1ad2a1a4c9d15fae5d3acd473bb1ab6b244ce
white <White_pozitiv@mail.ru>
2026-01-31 18:07:19 +0300

    flyingeTwo

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Тестовые данные");
+        System.out.println("Тестовые данные");//
         List<Flight> allFlights = FlightBuilder.createFlights();
         allFlights.forEach(System.out::println);
 
